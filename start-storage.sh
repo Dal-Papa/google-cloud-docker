ad7b6463258b135d1875d9184a587721563c3e38
Eliott Dumeix <eliott.dumeix@gmail.com>
2023-03-15 18:29:11 +0100

    add firebase storage emulator

---
+++ start-storage.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env sh
+
+# Create Firebase Storage config file.
+cat <<EOT >/firebase/storage.rules
+rules_version = '2';
+service firebase.storage {
+  match /b/{bucket}/o {
+    match /{allPaths=**} {
+      allow read, write: if true;
+    }
+  }
+}
+EOT
+
+# Create Firebase config file.
+cat <<EOT >/firebase/firebase.json
+{
+  "storage": {
+    "rules": "./storage.rules"
+  },
+  "emulators": {
+    "storage": {
+      "port": "${STORAGE_LISTEN_PORT}",
+      "host": "${FIREBASE_EMULATORS_HOST}"
+    }
+  }
+}
+EOT
+
+firebase emulators:start --project="${FIREBASE_PROJECT_ID}" --import /firebase/baseline-data
---
+++ storage.Dockerfile
@@ -0,0 +1,22 @@
+FROM node:lts-alpine3.13 AS app-env
+LABEL maintainer="Dal-Papa"
+
+RUN apk add --no-cache python3 py3-pip openjdk11-jre bash && \
+    npm install -g firebase-tools && \
+    firebase setup:emulators:storage && \
+    rm -rf /var/cache/apk/*
+
+ENV FIREBASE_PROJECT_ID="demo-dev"
+ENV FIREBASE_EMULATORS_HOST="0.0.0.0"
+ENV STORAGE_LISTEN_PORT="9199"
+EXPOSE $STORAGE_LISTEN_PORT
+
+RUN mkdir -p /firebase/baseline-data
+VOLUME /firebase
+
+COPY start-storage.sh /
+RUN chmod +x /start-storage.sh
+
+WORKDIR /firebase
+
+ENTRYPOINT ["/start-storage.sh"]
